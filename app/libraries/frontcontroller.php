76fc7e6d2c4397f183d7bad9cc0840e10c4951e7
Talal Danoon <tald7344@gmail.com>
2019-03-09 19:19:44 +0200

    Write parseUrl Method Inside Frontcontroller Class

--- app/libraries/frontcontroller.php
+++ app/libraries/frontcontroller.php
@@ -3,5 +3,27 @@
 	namespace PHPECOM\Libraries;
 
 	class FrontController {
+		private $_controllers = 'index',
+				$_actions = 'default',
+				$_params = array();
+
+		public function __construct() {
+			$this->parseUrl();
+		}
+
+		private function parseUrl() {
+			$url = explode('/', trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'), 3);
+			if (isset($url[0]) && $url[0] != '') {
+				$this->_controllers = $url[0];
+			}
+			if (isset($url[1]) && $url[1] != '') {
+				$this->_actions = $url[1];
+			}
+			if (isset($url[2]) && $url[2] != '') {
+				$this->_params = explode('/', $url[2]);
+			}
+		}
+
+
 		
 	}
