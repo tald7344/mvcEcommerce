1d0354afdac2b15e5994c55693cbc5b1cc4dc735
Talal Danoon <tald7344@gmail.com>
2019-03-12 18:29:08 +0200

    Build Abstract Model Class
    Build It And Make 'create' method inside it and test it

--- app/admin/models/abstractmodel.php
+++ app/admin/models/abstractmodel.php
@@ -1,16 +1,44 @@
 <?php
 
 	namespace PHPECOM\Admin\Models;
+	use PHPECOM\Libraries\Database\DatabaseHandler;
 
 	class AbstractModel {
 
-		const DATA_TYPE_STR = PDO::PARAM_STR;
-		const DATA_TYPE_INT = PDO::PARAM_INT;
-		const DATA_TYPE_BOOL = PDO::PARAM_BOOL;
-		
+		const DATA_TYPE_STR 	= \PDO::PARAM_STR;
+		const DATA_TYPE_INT 	= \PDO::PARAM_INT;
+		const DATA_TYPE_BOOL 	= \PDO::PARAM_BOOL;
+		const DATA_TYPE_DECIMAL = 4;
+
+
+		private static function getTableSchema() {
+			$output = '';
+			foreach (static::$_tableSchema as $columnName => $columnType) {
+				$output .= $columnName . ' = :' . $columnName . ', ';
+			}
+			return trim($output, ', ');
+		}
+
+		private function prepareBindValue(\PDOStatement &$stmt) {
+			foreach (static::$_tableSchema as $columnName => $columnType) {
+				if ($columnType == 4) {
+					$sanitizeColumn = filter_var($this->$columnName, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+					$stmt->bindValue(":{$columnName}", $sanitizeColumn);
+				} else {
+					$stmt->bindValue(":{$columnName}", $this->$columnName, $columnType);
+				}
+			}
+		}
 
 		public function create() {
-			$sql = 'INSERT INTO users SET username = :username, password = :password';
+			$sql = 'INSERT INTO ' . static::$_tableName . ' SET ' . self::getTableSchema();
+			$stmt = DatabaseHandler::getInstance()->prepare($sql);
+			$this->prepareBindValue($stmt);
+			if ($stmt->execute()) {
+				return true;
+			}
+			return false;
+		}
 
-		}	
+
 	}
